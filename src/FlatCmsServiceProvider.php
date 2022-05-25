b46f1f54920c3fd94d0bdf2ef4760e3c5236978c
Frédéric Blanc <frederic.blanc@itanea.fr>
2022-05-25 23:38:54 +0200

    Implements user model in Orbit mode without database. Just before trying to use Breeze with this flat model

--- src/FlatCmsServiceProvider.php
+++ src/FlatCmsServiceProvider.php
@@ -3,11 +3,30 @@
 namespace happytodev\FlatCms;
 
 use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
 use happytodev\FlatCms\Commands\FlatCmsCommand;
+use happytodev\FlatCms\Console\InstallFlatPackage;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class FlatCmsServiceProvider extends PackageServiceProvider
 {
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            // Create the user model and if necessary overwrite it
+            // @todo Find a better way to do this
+            // if (! class_exists('User')) {
+                $this->publishes([
+                    __DIR__ . '/Models/User.php' => 'app/Models/User.php',
+                ], 'models');
+            // }
+
+            // Install flat-cms command
+            $this->commands([
+                InstallFlatPackage::class,
+            ]);
+        }
+    }
+
     public function configurePackage(Package $package): void
     {
         /*
---
+++ src/Models/User.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Orbit\Concerns\Orbital;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Schema\Blueprint;
+
+class User extends Model
+{
+    use Orbital;
+
+    public static function schema(Blueprint $table)
+    {
+        $table->id();
+        $table->string('name');
+        $table->string('email')->unique();
+        $table->timestamp('email_verified_at')->nullable();
+        $table->string('password');
+        $table->rememberToken();
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
