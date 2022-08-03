a69a2ec0c10ed061d0dd05b91e04c4a5083f3bdd
Frédéric Blanc <frederic.blanc@itanea.fr>
2022-08-03 17:41:13 +0200

    Fix #20 Cleaning Try to launch some publishes after package updated

--- src/App/Filament/Resources/PostResource.php
+++ src/App/Filament/Resources/PostResource.php
@@ -32,31 +32,12 @@ class PostResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-newspaper';
 
-    // public static function form(Form $form): Form
-    // {
-    //     return $form
-    //         ->schema([
-    //             Forms\Components\TextInput::make('title')
-    //                 ->required(),
-    //             Forms\Components\TextInput::make('tldr'),
-    //             Forms\Components\TextInput::make('slug'),
-    //             Forms\Components\Textarea::make('content'),
-    //             BelongsToSelect::make('user_id')
-    //                 ->resource(UserResource::class)
-    //                 ->required(),
-    //             Forms\Components\TextInput::make('user_id')
-    //                 ->required(),
-    //         ]);
-    // }
-
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('title'),
                 Tables\Columns\TextColumn::make('tldr'),
-                //Tables\Columns\TextColumn::make('slug'),
-                //Tables\Columns\TextColumn::make('content'),
                 Tables\Columns\TextColumn::make('user_id'),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime(),
