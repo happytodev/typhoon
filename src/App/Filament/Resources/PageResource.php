85e9a7c4526fed28d3f6418ed372fceedc3d0b15
Frédéric Blanc <frederic.blanc@itanea.fr>
2022-08-14 00:17:52 +0200

    First implementation of plugins in Typhoon

--- src/App/Filament/Resources/PageResource.php
+++ src/App/Filament/Resources/PageResource.php
@@ -10,6 +10,8 @@ use Filament\Resources\Form;
 use Filament\Resources\Table;
 use Filament\Resources\Resource;
 use Filament\Forms\Components\Card;
+use Illuminate\Support\Facades\Log;
+use Filament\Forms\Components\Group;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Toggle;
 use Filament\Forms\Components\Builder;
@@ -24,6 +26,31 @@ class PageResource extends Resource
     protected static ?string $model = Page::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static $backgroundColors = [
+        'bg-white' => 'White',
+        'bg-amber-100' => 'Amber 100',
+        'bg-amber-300' => 'Amber 300',
+        'bg-amber-600' => 'Amber 600',
+        'bg-blue-100' => 'Blue 100',
+        'bg-blue-300' => 'Blue 300',
+        'bg-blue-600' => 'Blue 600',
+        'bg-green-100' => 'Green 100',
+        'bg-green-300' => 'Green 300',
+        'bg-green-600' => 'Green 600',
+        'bg-indigo-100' => 'Indigo 100',
+        'bg-indigo-300' => 'Indigo 300',
+        'bg-indigo-600' => 'Indigo 600',
+        'bg-lime-100' => 'Lime 100',
+        'bg-lime-300' => 'Lime 300',
+        'bg-lime-600' => 'Lime 600',
+        'bg-orange-100' => 'Orange 100',
+        'bg-orange-300' => 'Orange 300',
+        'bg-orange-600' => 'Orange 600',
+        'bg-yellow-100' => 'Yellow 100',
+        'bg-yellow-300' => 'Yellow 300',
+        'bg-yellow-600' => 'Yellow 600',
+    ];
 
     public static function form(Form $form): Form
     {
@@ -67,16 +94,7 @@ class PageResource extends Resource
                                     ])
                                     ->default('12'),
                                 Select::make('backgroundColor')
-                                    ->options([
-                                        'bg-white' => 'White',
-                                        'bg-amber-100' => 'Amber 100',
-                                        'bg-blue-100' => 'Blue 100',
-                                        'bg-green-100' => 'Green 100',
-                                        'bg-indigo-100' => 'Indigo 100',
-                                        'bg-lime-100' => 'Lime 100',
-                                        'bg-orange-100' => 'Orange 100',
-                                        'bg-yellow-100' => 'Yellow 100',
-                                    ])
+                                    ->options(self::$backgroundColors)
                                     ->default('bg-white'),
                                 Toggle::make('visible')
                                     ->label('Visible')
@@ -93,16 +111,7 @@ class PageResource extends Resource
                                 TextInput::make('featuredPostsNumber')
                                     ->label('Featured Post limit to display'),
                                 Select::make('backgroundColor')
-                                    ->options([
-                                        'bg-white' => 'White',
-                                        'bg-amber-100' => 'Amber 100',
-                                        'bg-blue-100' => 'Blue 100',
-                                        'bg-green-100' => 'Green 100',
-                                        'bg-indigo-100' => 'Indigo 100',
-                                        'bg-lime-100' => 'Lime 100',
-                                        'bg-orange-100' => 'Orange 100',
-                                        'bg-yellow-100' => 'Yellow 100',
-                                    ])
+                                    ->options(self::$backgroundColors)
                                     ->default('bg-white'),
                                 Toggle::make('visible')
                                     ->label('Visible')
@@ -119,16 +128,7 @@ class PageResource extends Resource
                                 TextInput::make('latestPostsNumber')
                                     ->label('Latest Post limit to display'),
                                 Select::make('backgroundColor')
-                                    ->options([
-                                        'bg-white' => 'White',
-                                        'bg-amber-100' => 'Amber 100',
-                                        'bg-blue-100' => 'Blue 100',
-                                        'bg-green-100' => 'Green 100',
-                                        'bg-indigo-100' => 'Indigo 100',
-                                        'bg-lime-100' => 'Lime 100',
-                                        'bg-orange-100' => 'Orange 100',
-                                        'bg-yellow-100' => 'Yellow 100',
-                                    ])
+                                    ->options(self::$backgroundColors)
                                     ->default('bg-white'),
                                 Toggle::make('visible')
                                         ->label('Visible')
@@ -140,21 +140,60 @@ class PageResource extends Resource
                                     ->label('Paragraph')
                                     ->required(),
                                 Select::make('backgroundColor')
-                                    ->options([
-                                        'bg-white' => 'White',
-                                        'bg-amber-100' => 'Amber 100',
-                                        'bg-blue-100' => 'Blue 100',
-                                        'bg-green-100' => 'Green 100',
-                                        'bg-indigo-100' => 'Indigo 100',
-                                        'bg-lime-100' => 'Lime 100',
-                                        'bg-orange-100' => 'Orange 100',
-                                        'bg-yellow-100' => 'Yellow 100',
-                                    ])
+                                    ->options(self::$backgroundColors)
                                     ->default('bg-white'),
                                 Toggle::make('visible')
                                         ->label('Visible')
                                         ->default(true)
                             ]),
+                        Builder\Block::make('plugins')
+                            ->schema(function () {
+                                // get the list of installed plugins
+                                $listPlugins = config('typhoon.plugins');
+                                return [
+                                    // display the list of available plugins
+                                    Select::make('type')
+                                    ->options(
+                                        $listPlugins
+                                    )
+                                    ->reactive(),
+                                    // by matching type of plugins :
+                                    // - instantiate the corresponding model
+                                    // - get all instances of this model
+                                    // - add them to the select input
+                                    Group::make()
+                                    ->schema(function ($get) {
+                                        $type = $get('type');
+                                        $activated = '';
+                                        if ($type != null) {
+                                            $modelName = 'App\Models\\' . Str::ucfirst($type);
+                                            $model = new $modelName();
+                                            $items = $model::class::all();
+
+                                            foreach ($items as $item) {
+                                                # code...
+                                                $listItems[$item->id] = $item->title;
+                                            }
+
+                                            $activated = 'activated';
+                                        }
+                                        return match ($activated) {
+                                            'activated' => [
+                                                Select::make('id')
+                                                ->options($listItems),
+                                            ],
+                                            default => [],
+                                        };
+                                    }),
+                                    Select::make('backgroundColor')
+                                    ->options(self::$backgroundColors)
+                                    ->default('bg-white'),
+                                    Toggle::make('visible')
+                                    ->label('Visible')
+                                    ->default(false)
+                                    ->columnSpan('full'),
+                                ];
+                            }),
                         Builder\Block::make('image')
                             ->schema([
                                 FileUpload::make('url')
@@ -165,16 +204,7 @@ class PageResource extends Resource
                                     ->label('Alt text')
                                     ->required(),
                                 Select::make('backgroundColor')
-                                    ->options([
-                                        'bg-white' => 'White',
-                                        'bg-amber-100' => 'Amber 100',
-                                        'bg-blue-100' => 'Blue 100',
-                                        'bg-green-100' => 'Green 100',
-                                        'bg-indigo-100' => 'Indigo 100',
-                                        'bg-lime-100' => 'Lime 100',
-                                        'bg-orange-100' => 'Orange 100',
-                                        'bg-yellow-100' => 'Yellow 100',
-                                    ])
+                                    ->options(self::$backgroundColors)
                                     ->default('bg-white'),
                                 Select::make('width')
                                     ->options([
@@ -207,30 +237,7 @@ class PageResource extends Resource
                                     ])
                                     ->default('right'),
                                 Select::make('backgroundColor')
-                                    ->options([
-                                        'bg-white' => 'White',
-                                        'bg-amber-100' => 'Amber 100',
-                                        'bg-amber-300' => 'Amber 300',
-                                        'bg-amber-600' => 'Amber 600',
-                                        'bg-blue-100' => 'Blue 100',
-                                        'bg-blue-300' => 'Blue 300',
-                                        'bg-blue-600' => 'Blue 600',
-                                        'bg-green-100' => 'Green 100',
-                                        'bg-green-300' => 'Green 300',
-                                        'bg-green-600' => 'Green 600',
-                                        'bg-indigo-100' => 'Indigo 100',
-                                        'bg-indigo-300' => 'Indigo 300',
-                                        'bg-indigo-600' => 'Indigo 600',
-                                        'bg-lime-100' => 'Lime 100',
-                                        'bg-lime-300' => 'Lime 300',
-                                        'bg-lime-600' => 'Lime 600',
-                                        'bg-orange-100' => 'Orange 100',
-                                        'bg-orange-300' => 'Orange 300',
-                                        'bg-orange-600' => 'Orange 600',
-                                        'bg-yellow-100' => 'Yellow 100',
-                                        'bg-yellow-300' => 'Yellow 300',
-                                        'bg-yellow-600' => 'Yellow 600',
-                                    ])
+                                    ->options(self::$backgroundColors)
                                     ->default('bg-white'),
                                 Toggle::make('visible')
                                         ->label('Visible')
