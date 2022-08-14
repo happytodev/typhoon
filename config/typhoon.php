8edd164696ad65ffb1eea0ce1d39de0946a62531
Frédéric Blanc <frederic.blanc@itanea.fr>
2022-08-14 13:34:55 +0200

    v0.5.4

--- config/typhoon.php
+++ config/typhoon.php
@@ -44,7 +44,7 @@ return [
     | Possibles values are the main colours of tailwindcss.
     |
     */
-    'version' => 'v0.5.2',
+    'version' => 'v0.5.4',
 
     /*
     |--------------------------------------------------------------------------
