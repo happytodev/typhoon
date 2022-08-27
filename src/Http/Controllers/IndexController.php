4e15b0a0ba76034b015f1134f64b6f29870ff899
Frédéric Blanc <frederic.blanc@itanea.fr>
2022-08-27 21:55:25 +0200

    Remove dead code

--- src/Http/Controllers/IndexController.php
+++ src/Http/Controllers/IndexController.php
@@ -18,46 +18,7 @@ class IndexController extends Controller
      */
     public function index()
     {
-        $rd1 = rand(1, 500);
 
-        // $user = new User();
-        // $user->name = "John Doe";
-        // $user->email = "jdoe$rd1@mail.com";
-        // $user->password = "secret";
-
-        try {
-            //code...
-            $userPassword = Hash::make('password');
-            $user = User::firstOrCreate(
-                ['name' => 'John Doe', 'email' => "jdoe$rd1@gmail.com", 'password' => $userPassword]
-            );
-
-            $post = new Post;
-            
-            $postRand = rand(1000, 9999);
-
-            $post->title = 'My post number ' . $postRand;
-            $post->tldr = 'This is the ' . $postRand . 'th post';
-            $post->slug = Str::slug($post->title);
-            $post->content = 'Lorem ipsum sit amet em dolor' . $postRand;
-            $post->user_id = $user->id;
-            $post->save();
-            dd($post);
-        } catch (\Throwable $th) {
-            //throw $th;
-            dd($th);
-        }
-        
-        echo 'test';
-        
-        try {
-            $result = $user->save();
-            var_dump( "Result: $result");
-            //code...
-        } catch (\Throwable $th) {
-            //throw $th;
-            dd($th);
-        } 
     }
 
     /**
