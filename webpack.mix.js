a80b0b094397ef7fac95f908e3cbea54ab5a75e5
Frédéric Blanc <frederic.blanc@itanea.fr>
2022-09-05 00:32:57 +0200

    WIP on CSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,3 +3,7 @@ const mix = require('laravel-mix')
 mix.postCss('resources/css/filament.css', 'public/css', [
     require('tailwindcss'), 
 ])
+
+mix.postCss('resources/css/app.css', 'public/css', [
+    require('tailwindcss'), 
+])
