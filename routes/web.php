2b8ffc244c181e2283e3f3d612adef327e57a9a4
Frédéric Blanc <frederic.blanc@itanea.fr>
2022-11-26 00:18:55 +0100

    Fix #47 Emergency fix, need time to search a better solution to personalize url

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,12 @@ use HappyToDev\Typhoon\Http\Controllers\IndexController;
 
 Route::middleware(['web'])->group(function () {
     
+    // Source of bug #47
     // define route for posts
-    $postsRoute = setting('posts.route') ?? 'posts';
+    // $postsRoute = setting('posts.route') ?? 'posts';
 
     // define route for pages
-    $pagesRoute = setting('pages.route') ?? 'page';
+    // $pagesRoute = setting('pages.route') ?? 'page';
  
     Route::get('/hometest', [HomeController::class, 'index'])->name('home');
 
@@ -20,11 +21,18 @@ Route::middleware(['web'])->group(function () {
 
     Route::get('/index', [IndexController::class, 'index'])->name('index.index');
 
-    Route::get("$postsRoute", [PostController::class, 'index'])->name('posts.index');
+    // Source of bug #47
+    // Route::get("$postsRoute", [PostController::class, 'index'])->name('posts.index');
 
-    Route::get("$postsRoute/{slug}", [PostController::class, 'show'])->name('posts.show');
+    // Route::get("$postsRoute/{slug}", [PostController::class, 'show'])->name('posts.show');
 
-    Route::get("$pagesRoute/{slug}", [PageController::class, 'index'])->name('page.index');
+    // Route::get("$pagesRoute/{slug}", [PageController::class, 'index'])->name('page.index');
+
+    Route::get("/posts/", [PostController::class, 'index'])->name('posts.index');
+
+    Route::get("/posts/{slug}", [PostController::class, 'show'])->name('posts.show');
+
+    Route::get("/page/{slug}", [PageController::class, 'index'])->name('page.index');
 });
 
 Route::redirect('/login', '/admin/login')->name('login');
